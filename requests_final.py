e54395f9b687f70c1d33b9bbb066532e44966413
Shalini Mustala <shalinimustala@Shalinis-MBP.attlocal.net>
2021-10-01 19:28:28 -0700

    requests

--- requests_final.py
+++ requests_final.py
@@ -10,7 +10,10 @@ import os
 working_directory = os.getcwd()
 import requests
 session = requests.session()
-url = 'https://journals.sagepub.com/action/doLogin?login=cbeckman%40price.usc.edu&password=3ditorAccess!'
+username = input("Enter username: ")
+password = input("Enter password:  ")
+
+url = 'https://journals.sagepub.com/action/doLogin?login=' + username + '&password=' + password
 response = session.get(url)
 
 with open('xml_urls.txt', 'r') as f:   # Journal Articles from 2011 to to date # Every year has 1 volume and 4 Issues (1,2,3,4) . 2011 is  Volume 56. 2020 is Volume 65 
